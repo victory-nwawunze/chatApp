c3d8826409e08ad80fd2bacce00a6dc8266482a0
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-16 08:40:59 -0700

    creating chatApp

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@
 import "./App.css";
 import chatFeed from "./components/chatFeed";
 import { ChatEngine, ChatFeed } from "react-chat-engine";
+import LoginForm from "./components/LoginForm";
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
-    <div>
-      <ChatEngine
-        height="100vh"
-        projectID="4d62209f-e762-4686-8176-192d1c0e2219"
-        userName="victor"
-        userSecret="1234"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-        onNewMessage={() =>
-          new Audio(
-            "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
-          ).play()
-        }
-      />
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="4d62209f-e762-4686-8176-192d1c0e2219"
+      userName="victor"
+      userSecret="1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
   );
 }
 
